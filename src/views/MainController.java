c20ea26367ad08f76b7b5851467f3b60090b5580
Brian <sukhoonchoi1@gmail.com>
2017-10-10 11:51:16 +1300

    Main Menu buttons change scenes into "old main menu" from assignment 3

--- src/views/MainController.java
+++ src/views/MainController.java
@@ -1,9 +1,17 @@
 package views;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,7 +28,25 @@ public class MainController implements Initializable {
     }
 
     @FXML
-    private void buttonPressed(){
+    private void buttonPressed(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+
+        if (event.getSource() == mathsAidButton){
+            //TODO Change to Maths aid Main Menu, and load controller
+            loader.setLocation(getClass().getResource("PracticeModule.fxml"));
+            loader.setController(new PracticeModuleController());
+        }
+
+        else if (event.getSource() == practiceButton){
+            loader.setLocation(getClass().getResource("PracticeModule.fxml"));
+            loader.setController(new PracticeModuleController());
+        }
+
+        Parent view = loader.load();
+        Scene playScene = new Scene(view);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(playScene);
+        window.show();
 
     }
 }
