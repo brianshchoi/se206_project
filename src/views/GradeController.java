f2580713f070b751a7676f9f68d59586486c7249
Jenny Lim <jlim064@aucklanduni.ac.nz>
2017-10-12 16:10:01 +1300

    only saves last 20 results + prarctice/math results saved to separately

--- src/views/GradeController.java
+++ src/views/GradeController.java
@@ -34,137 +34,152 @@ import java.util.ResourceBundle;
 
 public class GradeController implements Initializable{
 
-    @FXML
-    private Label totalScore;
-    @FXML
-    private Button playAgainEasy;
-    @FXML
-    private Button playAgainHard;
+	@FXML
+	private Label totalScore;
+	@FXML
+	private Button playAgainEasy;
+	@FXML
+	private Button playAgainHard;
 
-    //Define table for grade
-    @FXML
-    TableView<userTable> gradeTable;
-    @FXML
-    TableColumn<userTable, Integer> iTotal;
-    @FXML
-    TableColumn<userTable, String> iNickname;
-    @FXML
-    TableColumn<userTable, String> iDate;
+	//Define table for grade
+	@FXML
+	TableView<userTable> gradeTable;
+	@FXML
+	TableColumn<userTable, Integer> iTotal;
+	@FXML
+	TableColumn<userTable, String> iNickname;
+	@FXML
+	TableColumn<userTable, String> iDate;
 
-    private int score;
-    private String nickname;
-    private boolean hardLevel;
-    private boolean mathAid;
-    private static Main instance = Main.getInstance();
-    private final static int easyToHardBoundary = 8;
-    private final static int numQuestions = 10;
-    
-    private static final String FILENAME = "results.csv";
+	private int score;
+	private String nickname;
+	private boolean hardLevel;
+	private boolean mathAid;
+	private static Main instance = Main.getInstance();
+	private final static int easyToHardBoundary = 8;
+	private final static int numQuestions = 10;
+
+	private static final String MATH_FILE = "math_results.csv";
+	private static final String PRACTICE_FILE = "practice_results.csv";
+	private String FILENAME;
 
 
-    final ObservableList<userTable> data = FXCollections.observableArrayList();
+	final ObservableList<userTable> data = FXCollections.observableArrayList();
 
-    public void initData(int score, boolean hardLevel, boolean mathAid, String nickname){
-        this.score = score;
-        this.hardLevel = hardLevel;
-        this.mathAid = mathAid;
-        this.nickname = nickname;
-        Date date = new Date();
-        DateFormat dateFormat = new SimpleDateFormat("yy-mm-dd hh:mm");
-        String strDate = dateFormat.format(date);
-        loadDataFromFile();									// loads data from previous plays to the tableview
-        data.add(new userTable(score, nickname, strDate));	// adds the current play data to the tableview
-        try {
+	public void initData(int score, boolean hardLevel, boolean mathAid, String nickname){
+		this.score = score;
+		this.hardLevel = hardLevel;
+		this.mathAid = mathAid;
+		this.nickname = nickname;
+		if (mathAid) {
+			FILENAME = MATH_FILE;
+		} else {
+			FILENAME = PRACTICE_FILE;
+		}
+		Date date = new Date();
+		DateFormat dateFormat = new SimpleDateFormat("yy-mm-dd hh:mm");
+		String strDate = dateFormat.format(date);
+		loadDataFromFile();									// loads data from previous plays to the tableview
+		data.add(new userTable(score, nickname, strDate));	// adds the current play data to the tableview
+		try {
 			saveDataToFile();								// saves the current play data to the file
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-        
-    }
 
-    public void setData(){
-        totalScore.setText("Total: " + score + "/" + numQuestions);
-        if ((score >= easyToHardBoundary && !hardLevel)||(hardLevel)){
-            playAgainHard.setDisable(false);
-        } else {
-            playAgainHard.setDisable(true);
-        }
-    }
+	}
 
-    @FXML
-    private void mainMenuPressed(ActionEvent event) throws IOException {
-        instance.setMainScene();
-    }
+	public void setData(){
+		totalScore.setText("Total: " + score + "/" + numQuestions);
+		if ((score >= easyToHardBoundary && !hardLevel)||(hardLevel)){
+			playAgainHard.setDisable(false);
+		} else {
+			playAgainHard.setDisable(true);
+		}
+	}
 
-    @FXML
-    private void playAgainPressed(ActionEvent event) throws IOException {
-        ObservableList<Table> newData = FXCollections.observableArrayList();
+	@FXML
+	private void mainMenuPressed(ActionEvent event) throws IOException {
+		instance.setMainScene();
+	}
 
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("recordMenu.fxml"));
-        loader.setController(new RecordMenuController());
-        Parent view = loader.load();
+	@FXML
+	private void playAgainPressed(ActionEvent event) throws IOException {
+		ObservableList<Table> newData = FXCollections.observableArrayList();
 
-        Scene viewScene = new Scene(view);
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("recordMenu.fxml"));
+		loader.setController(new RecordMenuController());
+		Parent view = loader.load();
 
-        // Access the play view controller and call initData method
-        RecordMenuController controller = loader.getController();
+		Scene viewScene = new Scene(view);
 
-        if (event.getSource().equals(playAgainEasy)){
-            controller.initData(1,0,false, newData, mathAid, nickname);
-        } else if (event.getSource().equals(playAgainHard)){
-            controller.initData(1,0,true, newData, mathAid, nickname);
-        }
-        controller.setData();
+		// Access the play view controller and call initData method
+		RecordMenuController controller = loader.getController();
 
-        // Gets the stage information
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		if (event.getSource().equals(playAgainEasy)){
+			controller.initData(1,0,false, newData, mathAid, nickname);
+		} else if (event.getSource().equals(playAgainHard)){
+			controller.initData(1,0,true, newData, mathAid, nickname);
+		}
+		controller.setData();
 
-        window.setScene(viewScene);
-        window.show();
-    }
+		// Gets the stage information
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        iTotal.setCellValueFactory(new PropertyValueFactory<userTable, Integer>("Total"));
-        iNickname.setCellValueFactory(new PropertyValueFactory<userTable, String>("Nickname"));
-        iDate.setCellValueFactory(new PropertyValueFactory<userTable, String>("Date"));
-        gradeTable.setItems(data);
-    }
-    
-    /**
-     * Loads the saved data from previous plays.
-     */
-    public void loadDataFromFile() {
-    	try {
-    		File file = new File(FILENAME);
-    		file.createNewFile();				// Check for result data file existence and create one if it doesn't exist
-	        BufferedReader br = new BufferedReader(new FileReader(file));
-	        String line;
-	        String[] array;
+		window.setScene(viewScene);
+		window.show();
+	}
 
-	        while ((line = br.readLine()) != null){
-	            array = line.split(",");		// read the file, split it by ',' and put them into columns of the tableview
-	            gradeTable.getItems().add(new userTable(Integer.parseInt(array[0]), array[1], array[2]));
-	        }
-	        br.close();
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		iTotal.setCellValueFactory(new PropertyValueFactory<userTable, Integer>("Total"));
+		iNickname.setCellValueFactory(new PropertyValueFactory<userTable, String>("Nickname"));
+		iDate.setCellValueFactory(new PropertyValueFactory<userTable, String>("Date"));
+		gradeTable.setItems(data);
+	}
 
-	    }catch (Exception ex){
-	        ex.printStackTrace();
-	    }
-    }
+	/**
+	 * Loads the saved data from previous plays.
+	 */
+	public void loadDataFromFile() {
+		try {
+			File file = new File(FILENAME);
+			file.createNewFile();				// Check for result data file existence and create one if it doesn't exist
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+			String[] array;
 
-    /**
-     * Saves the current user results to the file.
-     */
-    public void saveDataToFile() throws IOException {
-    	Writer writer = null;
+			while ((line = br.readLine()) != null){
+				array = line.split(",");		// read the file, split it by ',' and put them into columns of the tableview
+				gradeTable.getItems().add(new userTable(Integer.parseInt(array[0]), array[1], array[2]));
+			}
+			br.close();
+
+		}catch (Exception ex){
+			ex.printStackTrace();
+		}
+	}
+
+	/**
+	 * Saves the current user results to the file.
+	 */
+	public void saveDataToFile() throws IOException {
+		Writer writer = null;
 		try {
 			File file = new File(FILENAME);
 			writer = new BufferedWriter(new FileWriter(file, true));
-			for (userTable userData : data) {
-				String text = userData.getTotal() + "," + userData.getNickname() + "," + userData.getDate() + "\n";
-				writer.write(text);
+			int size = data.size();					// only save last 20 results to the file
+			if (size < 21) {
+				for (int i = size; i > 0 ; i--) {
+					String text = data.get(i-1).getTotal().toString() + "," + data.get(i-1).getNickname() + "," + data.get(i-1).getDate() + "\n";
+					writer.write(text);
+				}
+			} else {
+				for (int i = size; i > size - 20 ; i--) {
+					String text = data.get(i-1).getTotal().toString() + "," + data.get(i-1).getNickname() + "," + data.get(i-1).getDate() + "\n";
+					writer.write(text);
+				}
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
@@ -173,5 +188,5 @@ public class GradeController implements Initializable{
 			writer.flush();
 			writer.close();
 		}
-    }
+	}
 }
